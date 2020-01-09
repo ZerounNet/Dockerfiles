65b219d23efee829ef344b002d5612d8402d0a02
ZerounNet <zerounnet@github.com>
2020-01-09 17:18:09 +0800

    init

--- Quanter-RealtimeCollector/setup.sh
+++ Quanter-RealtimeCollector/setup.sh
@@ -1,6 +1,9 @@
 tee /entrypoint.sh <<EOF
+sleep 15
 QARC_WEBSERVER& 
+sleep 5
 QARC_Stock&
+sleep 5
 qaps_sub --exchange stocktransaction --model fanout --host \$EventMQ_IP
 EOF
 chmod a+x /entrypoint.sh
